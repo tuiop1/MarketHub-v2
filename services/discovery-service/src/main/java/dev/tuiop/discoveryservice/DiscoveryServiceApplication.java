a379475749bb2dd0fa6aee7e25629b5e53d9477d
tuiop1 <tymurkurkov@gmail.com>
2026-07-22 20:37:47 +0200

    init discovery service

---
+++ services/discovery-service/src/main/java/dev/tuiop/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package dev.tuiop.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+
+     static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+
+}
